505be62397c9e349be9b5a1df2c7e47450805182
thedavidmeister <thedavidmeister@gmail.com>
2025-07-05 18:50:47 +0400

    docs

--- src/concrete/VerifyAlwaysApproved.sol
+++ src/concrete/VerifyAlwaysApproved.sol
@@ -3,6 +3,11 @@ pragma solidity =0.8.25;
 
 import {IVerifyV1, VerifyStatus, VERIFY_STATUS_APPROVED} from "src/interface/IVerifyV1.sol";
 
+/// @title VerifyAlwaysApproved
+/// @notice A concrete implementation of `IVerifyV1` that always returns
+/// `VERIFY_STATUS_APPROVED` for any account at any timestamp. This can generally
+/// be used as a "no-op" verifier that approves all accounts without any
+/// conditions or checks.
 contract VerifyAlwaysApproved is IVerifyV1 {
     /// @notice Always returns `VERIFY_STATUS_APPROVED` for any account.
     /// @param account The account to check the status of.
