a9298c494039f1997a6bfe7d7761ac08718b69b6
thedavidmeister <thedavidmeister@gmail.com>
2025-07-05 18:50:02 +0400

    always verified contract

---
+++ src/concrete/VerifyAlwaysApproved.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.25;
+
+import {IVerifyV1, VerifyStatus, VERIFY_STATUS_APPROVED} from "src/interface/IVerifyV1.sol";
+
+contract VerifyAlwaysApproved is IVerifyV1 {
+    /// @notice Always returns `VERIFY_STATUS_APPROVED` for any account.
+    /// @param account The account to check the status of.
+    /// @param timestamp The timestamp to check the status at.
+    /// @return The status of the account at the given timestamp.
+    function accountStatusAtTime(address account, uint256 timestamp) external pure override returns (VerifyStatus) {
+        return VERIFY_STATUS_APPROVED;
+    }
+}
